5f89e1ae74985b9d45ea2f8cc0bb159b786de8f3
Dima <dimakurinnoy8@gmail.com>
2024-07-25 22:37:35 +0200

    correct tiker

--- src/components/Tiker/Tiker.jsx
+++ src/components/Tiker/Tiker.jsx
@@ -10,8 +10,8 @@ import Featuret6 from '../../assets/Featured/Cointelegraph.webp'
 import Featuret7 from '../../assets/Featured/The_Financial_Express_logo.png'
 import Featuret8 from '../../assets/Featured/beincrypto_the_latest_high_quality_blockchain_and_cryptocurrency.png'
 import Featuret9 from '../../assets/Featured/coinsniper.png'
-import Featuret10 from '../../assets/Featured/png_clipart_logo_investing_com_product_brand_investment_logo_investingcom.png'
-import Featuret11 from '../../assets/Featured/png_transparent_forbes_chief_executive_united_states_logo_art_director.png'
+import Featuret10 from '../../assets/Featured/CoinGApe.png'
+import Featuret11 from '../../assets/Featured/crypto-news-flash-logo-2x.webp'
 
 import { Animate } from '../Header/Animate';
 import style from './Tiker.module.scss';
@@ -26,14 +26,16 @@ export const Tiker = () => {
       <div className={style.ticker}>
         <Marquee gradient={true} gradientColor='black'>
           <div className={style.ticker_wrapper}>
-            <img src={Featuret1} alt="" />
+            <img src={Featuret1} alt=""  style={{width:'300px'}} />
             
             
-            <img src={Featuret4} alt="" style={{width:'270px',height:'150px'}} />
-            <img src={Featuret5} alt=""  style={{width:'270px',height:'100px'}}/>
+            <img src={Featuret4} alt="" style={{width:'270px',height:'190px'}} />
+           
             <img src={Featuret6} alt="" style={{width:'265px',height:'80px'}} />
             <img src={Featuret7} alt="" style={{width:'465px',height:'70px'}}/>
-            <img src={Featuret9} alt="" style={{width:'265px',height:'70px'}}/>
+            <img src={Featuret9} alt="" style={{width:'380px',height:'70px'}}/>
+            <img src={Featuret10} alt=""  style={{height:'70px',width:'315px'}} />
+            <img src={Featuret11} alt=""  style={{height:'70px',width:'246px'}} />
             
           </div>
         </Marquee>
